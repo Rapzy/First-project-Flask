f0e507e6d0506a5be355f7b952d26301553e511d
rapzy <andrushko.egor@gmail.com>
2018-10-21 13:39:17 +0300

    Add Blueprint

--- view.py
+++ view.py
@@ -1,5 +1,7 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    name = 'Egor'
+    return render_template('index.html',name=name)
