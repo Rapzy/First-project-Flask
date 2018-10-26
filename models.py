b5640cc640db30a6bfcf70b5c6d8a7020a704fad
rapzy <andrushko.egor@gmail.com>
2018-10-26 17:31:08 +0300

    Add Tags

--- models.py
+++ models.py
@@ -2,9 +2,19 @@ from app import db
 from datetime import datetime
 import re
 
-def slugify(str):
+
+def slugify(title):
     pattern = r'[^\w+]'
-    return re.sub(pattern, '-', str)
+    title = re.sub(' ', '', title)
+    slug = re.sub(pattern, '-', title)
+    return "{}".format(slug)
+
+
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('tag.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('post.id'))
+                     )
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +22,8 @@ class Post(db.Model):
     slug = db.Column(db.String(140), unique=True)
     body = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now())
+
+    tags = db.relation('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
 
     def __init__(self, *args, **kwargs):
         super(Post, self).__init__(*args, **kwargs)
@@ -23,3 +35,17 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post id: {}, title: {}>'.format(self.id, self.title)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    slug = db.Column(db.String(100), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag id: {}, name: {}>'.format(self.id, self.name)
+
--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,16 +1,24 @@
 from flask import Blueprint
 from flask import render_template
 
-from models import Post
+from models import Post, Tag
 
-posts = Blueprint('posts', __name__,template_folder='templates')
+posts = Blueprint('posts', __name__, template_folder='templates')
+
 
 @posts.route('/')
 def index():
     posts = Post.query.all()
     return render_template('posts/index.html', posts=posts)
 
+
 @posts.route('/<slug>')
 def post_detail(slug):
     post = Post.query.filter(Post.slug == slug).first()
     return render_template('posts/post_detail.html', post=post)
+
+
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first()
+    return render_template('posts/tag_detail.html',tag=tag)
