405d07c2171cc595c9d95cf885914d8b1c6c7bdf
rapzy <andrushko.egor@gmail.com>
2018-10-29 16:51:52 +0300

    Add new post form

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -2,10 +2,32 @@ from flask import Blueprint
 from flask import render_template
 
 from models import Post, Tag
+from app import db
 
 from flask import request
 
+from .forms import PostForm
+
+from flask import redirect, url_for
+
 posts = Blueprint('posts', __name__, template_folder='templates')
+
+
+@posts.route('/create', methods=["POST", "GET"])
+def create_post():
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Error')
+        return redirect(url_for('posts.index'))
+    else:
+        form = PostForm()
+        return render_template('posts/create_post.html', form=form)
 
 
 @posts.route('/')
