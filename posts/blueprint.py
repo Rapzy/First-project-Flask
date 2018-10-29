bcc968104f2e03f11e2c322cd2236496e2156af6
rapzy <andrushko.egor@gmail.com>
2018-10-29 15:58:30 +0300

    Add search

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -3,12 +3,18 @@ from flask import render_template
 
 from models import Post, Tag
 
+from flask import request
+
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    query = request.args.get('query')
+    if query:
+        posts = Post.query.filter(Post.title.contains(query) | Post.body.contains(query)).all()
+    else:
+        posts = Post.query.all()
     return render_template('posts/index.html', posts=posts)
 
 
@@ -21,4 +27,4 @@ def post_detail(slug):
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug == slug).first()
-    return render_template('posts/tag_detail.html',tag=tag)
+    return render_template('posts/tag_detail.html', tag=tag)
